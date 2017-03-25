455d3b92f04801cfcdc036ef7c8e703d0180a8a4
Ed Stack <edward@primovisto.com>
2017-03-25 17:26:26 +0000

    Created Preloader

---
+++ script.js
@@ -0,0 +1,12 @@
+/*-----------------
+    Loader
+    -----------------*/
+
+$(document).ready(function() {
+
+    setTimeout(function() {
+        $('body').addClass('loaded');
+        $('h1').css('color', '#222222');
+    }, 3000);
+
+});
